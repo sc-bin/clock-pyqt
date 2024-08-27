9807f3844cb18df551d03f6df2947552cf989727
sc-bin <3335447573@qq.com>
2024-08-27 11:31:23 +0800

    创建sqlite数据表保存实时温湿度数据

--- src/sensor_hass_temp_xiaomi.py
+++ src/sensor_hass_temp_xiaomi.py
@@ -12,17 +12,79 @@ PASSWORD = "pi"  # 用户名对应的密码
 topic = "homeassistant/#"
 
 
-class SENSOR_TEMP:
+class SENSOR_DATE:
+    """
+    创建sqlite表格，用于存储与查找数据
+    """
+
     id: str
+    table_name_humidity_raw: str
 
     def __init__(self, id_num: str) -> None:
         self.id = id_num
+        self.table_name_humidity_raw = "s_" + id_num + "_humidity"
+        self.table_name_temperature_raw = "s_" + id_num + "_temperature"
+
+    def _db_exe(self, cmd, is_today: bool = True):
+        """
+        自动创建以日期为名的sql文件,并在其中执行传入的命令
+        """
+        yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+
+        today = datetime.now().strftime("%Y-%m-%d")
+        day = None
+        if is_today:
+            day = today
+        else:
+            day = yesterday
+
+        filename = "db/" + day + ".db"
+        folder = os.path.exists("./db")
+        if not folder:  # 判断是否存在文件夹如果不存在则创建为文件夹
+            os.makedirs("./db")  # makedirs 创建文件时如果路径不存在会创建这个路径
+        conn = sqlite3.connect(filename, check_same_thread=False)
+
+        cursor = conn.cursor()
+        cursor.execute(cmd)
+        conn.commit()  # 提交事务
+        value = cursor.fetchall()
+        conn.close()
+        return value
+
+    def _db_create_page(self, page_name) -> None:
+        """
+        尝试在今日sql文件中创建对应页
+        """
+        cmd = """
+                CREATE TABLE {0} (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    flag INTEGER,
+                    value INTEGER
+                )
+            """.format(
+            page_name
+        )
+        try:
+            return self._db_exe(cmd)
+        except:
+            return None
+
+    def _db_page_add_date(self, page_name, flag, value):
+        return self._db_exe(
+            "INSERT INTO {0} (flag, value) VALUES ({1}, {2})".format(
+                page_name, flag, value
+            )
+        )
 
     def add_humidity(self, value) -> None:
-        print("%s : 湿度 %s " % (self.id, value))
+        print("%s : humidity %s " % (self.id, value))
+        self._db_create_page(self.table_name_humidity_raw)
+        self._db_page_add_date(self.table_name_humidity_raw, time.time(), value)
 
     def add_temperature(self, value) -> None:
-        print("%s : 温度 %s " % (self.id, value))
+        print("%s : temperature %s " % (self.id, value))
+        self._db_create_page(self.table_name_temperature_raw)
+        self._db_page_add_date(self.table_name_temperature_raw, time.time(), value)
 
 
 def get_str_mid(str_raw: str, str1: str, str2: str):
@@ -44,7 +106,7 @@ class DEVICE_HASS_XIAOMI_TEMP_1:
     通过hass的mqtt转发功能,获取小米温湿度计1的信息
     """
 
-    sensors = {"0": SENSOR_TEMP("0")}
+    sensors = {"0": SENSOR_DATE("0")}
 
     flag = ""
     table_1s = ""
@@ -65,19 +127,19 @@ class DEVICE_HASS_XIAOMI_TEMP_1:
 
     def on_message(self, client, userdata, msg):
         text = str(msg.payload, "utf-8")
-        self.fetch(msg.topic, text)
+        self.fetch_humidity(msg.topic, text)
         self.fetch_temperature(msg.topic, text)
         # print("主题：", msg.topic)
         # print("消息：\r\n",  text,'\n' )
 
-    def fetch(self, topic: str, text: str):
+    def fetch_humidity(self, topic: str, text: str):
         id = get_str_mid(
             topic, "homeassistant/sensor/temperature_humidity_sensor_", "_humidity"
         )
         if id == None:
             return
         if id not in self.sensors.keys():
-            self.sensors[id] = SENSOR_TEMP(id)
+            self.sensors[id] = SENSOR_DATE(id)
 
         if (
             topic
@@ -94,7 +156,7 @@ class DEVICE_HASS_XIAOMI_TEMP_1:
         if id == None:
             return
         if id not in self.sensors.keys():
-            self.sensors[id] = SENSOR_TEMP(id)
+            self.sensors[id] = SENSOR_DATE(id)
 
         if (
             topic
@@ -105,10 +167,6 @@ class DEVICE_HASS_XIAOMI_TEMP_1:
             self.sensors[id].add_temperature(text)
 
     def __init__(self) -> None:
-        # self.flag = str
-        # self.table_1s = str + "_s"
-        # self.table_15min = str + "_15min"
-
         self.client = mqtt.Client()
         self.client.on_connect = self.on_connect  # 注册返回连接状态的回调函数
         self.client.username_pw_set(USER, PASSWORD)  # 如果服务器要求需要账户密码
@@ -125,133 +183,6 @@ class DEVICE_HASS_XIAOMI_TEMP_1:
         self.client.subscribe(topic, qos=0)  # 订阅主题test/#
         self.client.loop_start()  # 非阻塞，启动接收线程
         # client.loop_forever()              #阻塞式，会卡死在这等待接收
-
-    # def __db_exe(self, str, is_today: bool = True):
-
-    #     yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
-
-    #     today = datetime.now().strftime("%Y-%m-%d")
-    #     day = None
-    #     if is_today:
-    #         day = today
-    #     else:
-    #         day = yesterday
-
-    #     filename = "db/" + day + ".db"
-    #     folder = os.path.exists("./db")
-    #     if not folder:  # 判断是否存在文件夹如果不存在则创建为文件夹
-    #         os.makedirs("./db")  # makedirs 创建文件时如果路径不存在会创建这个路径
-    #     conn = sqlite3.connect(filename, check_same_thread=False)
-
-    #     cursor = conn.cursor()
-    #     # print(str)
-    #     cursor.execute(str)
-    #     conn.commit()  # 提交事务
-    #     value = cursor.fetchall()
-    #     conn.close()
-    #     return value
-
-    # def __db_creat_table(self, name):
-    #     try:
-    #         self.__db_exe(
-    #             """
-    #             CREATE TABLE {0} (
-    #                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-    #                 time INTEGER,
-    #                 temp INTEGER
-    #             )
-    #         """.format(
-    #                 name
-    #             )
-    #         )
-    #     except:
-    #         pass
-
-    # def min15_today(self) -> list:
-    #     data = [0 for i in range(95)]
-    #     try:
-    #         value = self.__db_exe(
-    #             "SELECT * FROM {0} ORDER BY id DESC LIMIT 96".format(self.table_15min)
-    #         )
-    #         for i in value:
-    #             # data.append(i)
-    #             data[i[1]] = i[2] / 100
-    #     except:
-    #         pass
-    #     return data
-
-    # def min15_yesterday(self) -> list:
-    #     data = [0 for i in range(95)]
-    #     try:
-    #         value = self.__db_exe(
-    #             "SELECT * FROM {0} ORDER BY id DESC LIMIT 96".format(self.table_15min),
-    #             False,
-    #         )
-    #         for i in value:
-    #             # data.append(i)
-    #             data[i[1]] = i[2] / 100
-    #     except:
-    #         pass
-    #     return data
-
-    # def get_temp(self) -> str:
-    #     return self.__now_temp[0:-2] + "." + self.__now_temp[-2:]
-
-    # def fetch(self, msg):
-    #     pattern = "Temp \[{0}\] = \[(\d+)\]".format(self.flag)
-    #     match = re.search(pattern, msg)
-    #     if match:
-    #         temp = match.group(1)
-    #         self.__now_temp = temp
-    #         # print(self.flag, "=", temp)
-    #         self.__db_creat_table(self.table_1s)
-    #         self.__db_exe(
-    #             "INSERT INTO {0} (time, temp) VALUES ({1}, {2})".format(
-    #                 self.table_1s, time.time(), temp
-    #             )
-    #         )
-
-    #     # 计算上一个已经完整度过的15分钟区间的开始和结束时间
-
-    #     now = datetime.now()
-    #     end_time = now - timedelta(
-    #         minutes=now.minute % 15, seconds=now.second, microseconds=now.microsecond
-    #     )
-    #     start_time = end_time - timedelta(minutes=15)
-    #     end_time = end_time.timestamp()
-    #     start_time = start_time.timestamp()
-    #     # print(end_time)
-    #     # print(start_time)
-    #     current_interval = now.hour * 4 + now.minute // 15
-    #     last_interval = current_interval - 1
-    #     # print("当前区间是", current_interval)
-
-    #     self.__db_creat_table(self.table_15min)
-    #     try:
-    #         avg = self.__db_exe(
-    #             "SELECT AVG(temp) FROM {0} WHERE time BETWEEN {1} AND {2}".format(
-    #                 self.table_1s, start_time, end_time
-    #             )
-    #         )
-    #         avg = int(avg[0][0])
-    #         # print(f'Average temperature in the last completed 15-minute interval: {avg}')
-
-    #         # 检查temp_15min表中是否有一行的time列等于last_interval
-    #         # 如果没有这样的行，则插入一行新数据
-    #         tmp = self.__db_exe(
-    #             "SELECT * FROM {0} WHERE time = {1}".format(
-    #                 self.table_15min, last_interval
-    #             )
-    #         )
-    #         if len(tmp) == 0:
-    #             # print("插入15min新行")
-    #             self.__db_exe(
-    #                 "INSERT INTO {0} (time, temp) VALUES ({1}, {2})".format(
-    #                     self.table_15min, last_interval, avg
-    #                 )
-    #             )
-    #     except:
-    #         pass
 
 
 xiaomi_temp = DEVICE_HASS_XIAOMI_TEMP_1()
